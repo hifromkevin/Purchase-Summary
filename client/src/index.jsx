414a75bd1b9db9a705df775972961a85647c7bca
Kevin Lam <lam.kevin86@gmail.com>
2018-05-15 16:48:50 -0700

    Updates basic styling of application

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,14 +6,47 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      testing: 'Test!'
+      testing: 'Test!',
+      zip: undefined,
+
     }
   }
 
   render() {
     return (
-      <div>
-        <h1>Hi from App.jsx!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-8 pos-border">
+            <h2 className="title-height">Purchase Summary</h2>
+            <div className="row">
+              <div className="col-8">Subtotal</div>
+              <div className="col-4 text-right totals">$123.45</div>
+            </div>
+            <div className="row">
+              <div className="col-8">Pick-Up Savings</div>
+              <div className="col-4 text-right savings totals">-$1.23</div>
+            </div>
+            <div className="row">
+              <div className="col-8">Est. Tax and Fees<br />(Based on ZIPCODE)</div>
+              <div className="col-4 text-right totals">$8.92</div>
+            </div>
+            <hr />
+            <div className="row">
+              <div className="col-8"><h3 className="title-height">Est. Total</h3></div>
+              <div className="col-4 text-right totals"><h3>$8.92</h3></div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                See Item Details <div className="plus"><span></span><span></span></div>
+              </div>
+            </div>
+
+            <hr />
+            <div className="row">
+               <div className="col-12">Apply Promo Code</div>
+            </div>
+          </div>
+        </div>
         <p><Text testing={this.state} /></p>
       </div>
     )
